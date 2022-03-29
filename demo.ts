a512ca90f6d8af427e486a190ed3eb8a3f3a8a51
wangheng87 <82043525@163.com>
2022-03-29 10:20:46 +0800

    third

--- demo.ts
+++
@@ -1,8 +0,0 @@
-var message:string = "Hello World";
-console.log(message)
-
-
-var a = dom.format("Result is #{0},#{1}", 22,33);
-alert(a);//"Result is 22,33"
-var b = dom.format("#{name} is a #{sex}",{name:"Jhon",sex:"man"});
-alert(b);//"Jhon is a man"
